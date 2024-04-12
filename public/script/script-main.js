44f1b42ddf1e855497b4222f35483488f023e2c8
Stephdev48 <147699498+Stephdev48@users.noreply.github.com>
2024-04-12 10:59:31 +0200

    divers

---
+++ public/script/script-main.js
@@ -0,0 +1,3 @@
+function alertTest(){
+    alert("Test ça marche !");
+}
--- views/base.view.php
+++ views/base.view.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="../public/script/script-main.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
     <title>Document</title>
 </head>
@@ -11,6 +12,8 @@
     <h1 class="text-3xl font-bold underline">
     Hello world!
     </h1>
+    <img src="../public/pictures/cc-assur.jpeg" alt="test-pic"/>
+    <script>alertTest();</script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
 </body>
-</html>  
+</html>

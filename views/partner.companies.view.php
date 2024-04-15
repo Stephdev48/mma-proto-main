ec715f3f752dbdfd96fb8fe0e52fd84458d1545d
Stephdev48 <147699498+Stephdev48@users.noreply.github.com>
2024-04-15 14:20:15 +0200

    Update partner.companies.view.php

--- views/partner.companies.view.php
+++ views/partner.companies.view.php
@@ -1,22 +1,38 @@
 
-<div class="">
-    <img src="../public/pictures/aig.jpg" alt="aig"/>
-    <img src="../public/pictures/albingia.jpg" alt="albingia"/>
-    <img src="../public/pictures/alptis.jpg" alt="alptis"/>
-    <img src="../public/pictures/apicil.png" alt="apicil"/>
-    <img src="../public/pictures/april.png" alt="april"/>
-    <img src="../public/pictures/auxiliaire.png" alt="auxiliaire"/>
-    <img src="../public/pictures/axre.png" alt="axre"/>
-    <img src="../public/pictures/groupama.png" alt="groupama"/>
-    <img src="../public/pictures/chubb.jpg" alt="chubb"/>
-    <img src="../public/pictures/generali.jpg" alt="generali"/>
-    <img src="../public/pictures/klarity.png" alt="klarity"/>
-    <img src="../public/pictures/europ-assistance.png" alt="europ-assistance"/>
-    <img src="../public/pictures/ergo.png" alt="ergo"/>
-    <img src="../public/pictures/entoria.jpg" alt="entoria"/>
-    <img src="../public/pictures/metlife.jpg" alt="helvetia-metlife"/>
-    <img src="../public/pictures/zephir.jpg" alt="groupe-zephir"/>
-    <img src="../public/pictures/netvox.jpg" alt="netvox"/>
-    <img src="../public/pictures/qbe.jpg" alt="qbe"/>
-    <img src="../public/pictures/tetris.png" alt="tetris"/>
+<?php
+    //Customized title
+    $title = "Partenaires";
+    
+    // Filling the content variable
+    ob_start();
+    ?>
+    
+    
+<div class="flex flex-wrap">
+    <img src="../public/pictures/aig.jpg" alt="aig" class="m-6 w-[200px]"/>
+    <img src="../public/pictures/albingia.jpg" alt="albingia" class="m-6 w-[200px]"/>
+    <img src="../public/pictures/alptis.jpg" alt="alptis" class="m-6 w-[200px]"/>
+    <img src="../public/pictures/apicil.png" alt="apicil" class="m-6 w-[200px]"/>
+    <img src="../public/pictures/april.png" alt="april" class="m-6 w-[200px]"/>
+    <img src="../public/pictures/auxiliaire.png" alt="auxiliaire" class="m-6 w-[200px]"/>
+    <img src="../public/pictures/axre.png" alt="axre" class="m-6 w-[200px]"/>
+    <img src="../public/pictures/groupama.png" alt="groupama" class="m-6 w-[200px]"/>
+    <img src="../public/pictures/chubb.jpg" alt="chubb" class="m-6 w-[200px]"/>
+    <img src="../public/pictures/generali.jpg" alt="generali" class="m-6 w-[200px]"/>
+    <img src="../public/pictures/klarity.png" alt="klarity" class="m-6 w-[200px]"/>
+    <img src="../public/pictures/europ-assistance.png" alt="europ-assistance" class="m-6 w-[200px]"/>
+    <img src="../public/pictures/ergo.png" alt="ergo" class="m-6 w-[200px]"/>
+    <img src="../public/pictures/entoria.jpg" alt="entoria" class="m-6 w-[200px]"/>
+    <img src="../public/pictures/metlife.jpg" alt="helvetia-metlife" class="m-6 w-[200px]"/>
+    <img src="../public/pictures/zephir.jpg" alt="groupe-zephir" class="m-6 w-[200px]"/>
+    <img src="../public/pictures/netvox.jpg" alt="netvox" class="m-6 w-[200px]"/>
+    <img src="../public/pictures/qbe.jpg" alt="qbe" class="m-6 w-[200px]"/>
+    <img src="../public/pictures/tetris.png" alt="tetris" class="m-6 w-[200px]"/>
 </div>
+
+
+
+<?php
+    $content = ob_get_clean();
+    require("base.view.php");
+    ?>

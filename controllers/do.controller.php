f04c13ef864d16649ddedb41034a83ef71a46635
ChrisGreta <c.leydier@velay.greta.fr>
2024-05-06 16:15:17 +0200

    DO une seul controller

---
+++ controllers/do.controller.php
@@ -0,0 +1,69 @@
+<?php
+    require_once 'models/form-entreprises.model.php'; 
+
+    function stepDisplay($currentstep){        
+
+        //var_dump($_SESSION);
+        // Remplissage de la variable $content
+        ob_start();
+        switch ($currentstep) {
+            case 'step1':
+                $title = "Formulaire DO-01";
+                require('views/s01-coordonnees.view.php');
+                $nextstep="step2";
+                break;
+            case 'step2':
+                $title = "Formulaire DO-02";
+                require('views/s02-maitre-ouvrage.view.php');
+                $nextstep="step3";
+                break;           
+            case 'step3':
+                $title = "Formulaire DO-03";
+                require('views/s03-oper-construct.view.php');
+                $nextstep="step4";
+                break;
+            case 'step4':
+                $title = "Formulaire DO-04";
+                require('views/s04-informations-diverses.view.php');
+                $nextstep="step4bis";  
+                break; 
+            case 'step4bis':
+                $title = "Formulaire DO-04bis";         
+                if($_SESSION["info-situation"]['situation-construction-bois']=="non"
+                || $_SESSION["info-situation"]['situation-pann-photo'] =="non" 
+                || $_SESSION["info-situation"]['situation-geothermie'] =="non" 
+                || $_SESSION["info-situation"]['situation-controle-tech'] =="non") {
+                    header("Location: index.php?page=step5");
+                }                         
+                $nextstep="step5";
+                require('views/s04-bis-travaux-annexes.view.php');     
+                break;                
+            case 'step5':
+                $title = "Formulaire DO-05";
+                require('views/s05-maitrise-oeuvre.view.php');
+                $nextstep="step6";
+                break;    
+            case 'step6':
+                $title = "Formulaire DO-06";
+                require('views/s06-cnr-risques-chantier.view.php');
+                $nextstep="validation";
+                break;                                             
+            default:
+                # code...
+                break;
+        }
+        
+        // Envoi des champs du formulaire
+        if (isset($_POST['fields'])) {
+            foreach ($_POST as $key => $value)
+            {
+                $_SESSION['info-'.$_POST['fields']][$key] = $value;
+            }
+            $keys = array_keys($_SESSION['info-'.$_POST['fields']]);
+            header("Location: index.php?page=".$nextstep);
+        }
+
+
+        $content = ob_get_clean();
+        require("views/base.view.php");
+    }
--- index.php
+++ index.php
@@ -5,14 +5,15 @@
     define('DEBUG', true );
 
     require 'controllers/home.controller.php';
-    require 'controllers/s01-coordonnees.controller.php';
+    require 'controllers/do.controller.php';
+    /*require 'controllers/s01-coordonnees.controller.php';
     require 'controllers/s02-maitre-ouvrage.controller.php';
     require 'controllers/s03-oper-construct.controller.php';
     require 'controllers/s04-informations-diverses.controller.php';
     require 'controllers/s04-bis-travaux-annexes.controller.php';
     require 'controllers/s05-maitrise-oeuvre.controller.php';
-    require 'controllers/s06-cnr-risques-chantier.controller.php';
-    require 'controllers/s08-intervenants-lots-techniques.php';
+    require 'controllers/s06-cnr-risques-chantier.controller.php';*/
+    
     require 'controllers/validation.controller.php';
     require 'controllers/page-erreur.controller.php';
    
@@ -29,32 +30,21 @@
                 homeDisplay($currentstep);
                 break;
             case 'step1':
-                step1Display($currentstep);
-                break;
-            case'step2':
-                step2Display($currentstep);
-                break;
+            case 'step2':
             case 'step3':
-                step3Display($currentstep);
-                break;
             case 'step4':
-                step4Display($currentstep);
-                break;
             case 'step4bis':
-                step4BisDisplay($currentstep);
-                break;
-            case 'step5':
-                step5Display($currentstep);
-                break;
+            case 'step5':                
             case 'step6':
-                step6Display($currentstep);
-                break;
-            case 'step8':
-                step8Display($currentstep);
-                break;                
+                stepDisplay($currentstep);
+                break;               
             case 'validation':
                 validDisplay($currentstep);
                 break;
+            case 'step8':
+                require 'controllers/s08-intervenants-lots-techniques.php';
+                step8Display($currentstep);
+                break;                 
             default:
                 throw new Exception ('Paramètre invalide !');
                 break;
